a4cdfaba43146ea528b0ab9edee4c8a0353969e0
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2023-04-30 00:28:13 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter path of file: ");
-        String path = sc.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter path to graph file: ");
+        String path = scanner.nextLine();
         Graph g = new Graph(path);
+
         int[] costs = new int[g.size()];
         int[] parents = new int[g.size()];
         boolean result;
 
-        //Dijkstra
-        g.dijkstra(0 , costs , parents);
-        System.out.println(Arrays.toString(costs));
-        System.out.println(Arrays.toString(parents));
+        // Test Dijkstra's algorithm
+        int source = 0;
+        //int[] costs = new int[g.size()];
+        //int[] parents = new int[g.size()];
+        g.dijkstra(source, costs, parents);
+        System.out.println("Dijkstra's algorithm results:");
+        System.out.println("Costs: " + Arrays.toString(costs));
+        System.out.println("Parents: " + Arrays.toString(parents));
 
-        //Bellman-ford
-        Arrays.fill(costs , Integer.MAX_VALUE);
-        Arrays.fill(parents, -1);
-        result = g.bellmanFord(0,costs , parents);
-        if(result){
-            System.out.println(Arrays.toString(costs));
-            System.out.println(Arrays.toString(parents));
-
-        }
-        else{
+        // Test Bellman-Ford algorithm
+        //int[] costs2 = new int[g.size()];
+        //int[] parents2 = new int[g.size()];
+        boolean hasNegativeCycle = g.bellmanFord(source, costs, parents);
+        System.out.println("bellmanFord's algorithm results:");
+        if (hasNegativeCycle) {
+            System.out.println("No negative cycle found");
+            System.out.println("Costs: " + Arrays.toString(costs));
+            System.out.println("Parents: " + Arrays.toString(parents));
+        } else {
             System.out.println("Negative cycle found");
         }
 
-
-        //Floyd-warshal
+        // Floyd-Warshall algorithm
         int[][] costsMatrix = new int[g.size()][g.size()];
         int[][] predecessorsMatrix = new int[g.size()][g.size()];
-        g.floydWarshall(costsMatrix,predecessorsMatrix);
-
-        for(int i = 0 ; i<g.size() ; i++){
-            System.out.println(Arrays.toString(costsMatrix[i]));
-
-        }
-        for(int i = 0 ; i<g.size(); i++){
-            System.out.println(Arrays.toString(predecessorsMatrix[i]));
+        result = g.floydWarshall(costsMatrix, predecessorsMatrix);
+        if (result) {
+            for (int i = 0; i < g.size(); i++) {
+                for (int j = 0; j < g.size(); j++) {
+                    System.out.print(costsMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < g.size(); i++) {
+                for (int j = 0; j < g.size(); j++) {
+                    System.out.print(predecessorsMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Negative cycle found");
         }
     }
 
