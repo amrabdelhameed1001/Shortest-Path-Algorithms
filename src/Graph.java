97f9b2dd76cd0d82e82cd7181f7252213cd74b6a
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2023-04-29 21:23:02 +0200

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -52,6 +52,67 @@ public class Graph {
         }
     }
 
+    public boolean bellmanFord(int source, int[] costs, int[] parents) {
+        // Initialize the costs array and parents array
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        Arrays.fill(parents, -1);
+        costs[source] = 0;
+
+        // Relax all edges V-1 times
+        for (int i = 0; i < V - 1; i++) {
+            for (Edge edge : edges) {
+                int u = edge.src;
+                int v = edge.dest;
+                int weight = edge.weight;
+                if (costs[u] != Integer.MAX_VALUE && costs[u] + weight < costs[v]) {
+                    costs[v] = costs[u] + weight;
+                    parents[v] = u;
+                }
+            }
+        }
+
+        // Check for negative cycles
+        for (Edge edge : edges) {
+            int u = edge.src;
+            int v = edge.dest;
+            int weight = edge.weight;
+            if (costs[u] != Integer.MAX_VALUE && costs[u] + weight < costs[v]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean floydWarshall(int[][] costs, int[][] predecessors) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                costs[i][j] = Integer.MAX_VALUE;
+                predecessors[i][j] = -1;
+            }
+            costs[i][i] = 0;
+        }
+        for (Edge e : edges) {
+            costs[e.src][e.dest] = e.weight;
+            predecessors[e.src][e.dest] = e.src;
+        }
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (costs[i][k] != Integer.MAX_VALUE && costs[k][j] != Integer.MAX_VALUE && costs[i][k] + costs[k][j] < costs[i][j]) {
+                        costs[i][j] = costs[i][k] + costs[k][j];
+                        predecessors[i][j] = predecessors[k][j];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < V; i++) {
+            if (costs[i][i] < 0) {
+                return false; // negative cycle found
+            }
+        }
+        return true;
+    }
     private int minDistance(int[] costs , boolean[] visited){
         int min = Integer.MAX_VALUE;
         int minIndex = -1;
